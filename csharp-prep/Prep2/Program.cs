019b821db88ab129eae3af3678cb00a5d9905df0
ebark24 <elibarker@outlook.com>
2023-04-20 11:56:10 -0600

    Created grade program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage:");
+        string grade = Console.ReadLine();
+        int grade_percentage = int.Parse(grade);
+        string letter = "no grade";
+        if (grade_percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade_percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade_percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade_percentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade_percentage < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade: {letter}");
+        if (grade_percentage >= 70)
+        {
+            Console.WriteLine("Congrats! You have passed the class!");
+        }
+        else 
+        {
+            Console.WriteLine("You have not passed the class, I'm sure you will do better next time!");
+        }
+
     }
 }
