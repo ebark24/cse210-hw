2344cc3611ef62c7c09374a9908634187d2dbc2d
ebark24 <elibarker@outlook.com>
2023-04-23 10:05:33 -0600

    Created number guessing program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1,100);
+        int guess = 0;
+        do
+        {
+            Console.WriteLine("What is your guess:");
+            guess = int.Parse(Console.ReadLine());
+            if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == magic_number)
+            {
+                Console.WriteLine("That is the right number");
+            }
+        } while (guess != magic_number);
+
     }
 }
