dbeedbd98922284afb5e5c23b9c5d7992ea751b4
ebark24 <elibarker@outlook.com>
2023-04-23 16:26:09 -0600

    Created Number Program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = 10;
+        List<int> numbers = new List<int>();
+        while (number != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum = sum + num;
+        }
+        Console.WriteLine(sum);
+
+        int average = sum / numbers.Count();
+        Console.WriteLine(average);
+
+        int large_number = 0;
+        foreach (int numb in numbers)
+        {
+            if (numb > large_number)
+            {
+                large_number = numb;
+            }
+        }
+        Console.WriteLine(large_number);
+
+
+
+
     }
 }
