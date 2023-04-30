a0959c45cefe833e090187c0efb04c5103ad0449
ebark24 <elibarker@outlook.com>
2023-04-30 16:42:13 -0600

    Created the Resume and Jobs class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job = new Job();
+        job._jobTitle = "Auditor";
+        job._company = "PwC";
+        job._startYear = 2021;
+        job._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Tax";
+        job2._company = "EY";
+        job2._startYear = 2018;
+        job2._endYear = 2010;
+
+        Resume myResume = new Resume();
+        myResume._name = "Eli Barker";
+        myResume._jobs.Add(job);
+        myResume._jobs.Add(job2);
+        myResume.displayJobs();
+
     }
 }
