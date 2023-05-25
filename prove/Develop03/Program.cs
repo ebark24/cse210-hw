77bb93e53f4d625253cbdeb2eec029660152f00b
ebark24 <elibarker@outlook.com>
2023-05-25 13:22:59 -0600

    Created List of Scriptures to pull from

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,9 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Scriputre scriputre1 = new Scriputre("2 Nephi","2","27","Wherefore, men are afree according to the bflesh; and call things are dgiven them which are expedient unto man. And they are free to echoose fliberty and eternal glife, through the great Mediator of all men, or to choose captivity and death, according to the captivity and power of the devil; for he seeketh that all men might be hmiserable like unto himself.");
+        List<Scriputre> SctiptureList = new List<Scriputre>();
+        Scriputre scriputre1 = new Scriputre("2 Nephi","2","27","Wherefore, men are free according to the flesh; and all things are given them which are expedient unto man. And they are free to choose liberty and eternal life, through the great Mediator of all men, or to choose captivity and death, according to the captivity and power of the devil; for he seeketh that all men might be miserable like unto himself.");
+        Scriputre scriputre2 = new Scriputre("Jeramiah","17","7","Blessed is the man that trusteth in the Lord, and whose hope the Lord is.");
+        Scriputre scriputre3 = new Scriputre("Matthew","11","28","30","Come unto me, all ye that labour and are heavy laden, and I will give you rest. Take my yoke upon you, and learn of me; for I am meek and lowly in heart: and ye shall find rest unto your souls. For my yoke is easy, and my burden is light");
+        Scriputre scriputre4 = new Scriputre("1 John","3","2","3","Beloved, now are we the sons of God, and it doth not yet appear what we shall be: but we know that, when he shall appear, we shall be like him; for we shall see him as he is. And every man that hath this hope in him purifieth himself, even as he is pure.");
+        SctiptureList.Add(scriputre1);
+        SctiptureList.Add(scriputre2);
+        SctiptureList.Add(scriputre3);
+        SctiptureList.Add(scriputre4);
 
-        scriputre1.DisplayScripture();
+        Random rnd = new Random();
+        int randIndex = rnd.Next(SctiptureList.Count);
+        Scriputre randomScripture = SctiptureList[randIndex];
+        
+        
+        Console.Clear();
+        randomScripture.DisplayScripture();
         Console.WriteLine("");
         Console.WriteLine("Press Enter to continue or type 'Quit' to finish");
 
@@ -14,13 +28,13 @@ class Program
 
         while (UserInput != "Quit")
         {
-            int WordsLeftToBeChanged = scriputre1.GetWordsToBeChangedCount();
+            int WordsLeftToBeChanged = randomScripture.GetWordsToBeChangedCount();
             if (WordsLeftToBeChanged > 0)
             {
                 UserInput = Console.ReadLine();
                 Console.Clear();
-                scriputre1.MakeMultipleBlanks();
-                scriputre1.DisplayScripture();
+                randomScripture.MakeMultipleBlanks();
+                randomScripture.DisplayScripture();
                 Console.WriteLine("");
                 Console.WriteLine("Press Enter to continue or type 'Quit' to finish");
             }
