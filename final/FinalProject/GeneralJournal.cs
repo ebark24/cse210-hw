d9429a9c3946a78281dfaffbaf7311b8e94e25db
ebark24 <elibarker@outlook.com>
2023-07-11 15:10:06 -0600

    Made the baiscs for loading

--- final/FinalProject/GeneralJournal.cs
+++ final/FinalProject/GeneralJournal.cs
@@ -26,8 +26,19 @@ public class GeneralJournal
         foreach(string line in lines)
         {
             string[] Sections = line.Split(":");
-            string[] Dailyinfo = Sections[0].Split("|");
-            string[] Diet = Sections[1].Split("|");
+            string[] DailyinfoP = Sections[0].Split("|");
+            string[] DietP = Sections[1].Split("|");
+            string[] WaterLogP = Sections[2].Split("|");
+            string[] ReadingP = Sections[3].Split("|");
+            string[] Workout1P = Sections[4].Split("|");
+            string[] workout2P = Sections[5].Split("|");
+            Workout workout1 = new Workout(Workout1P[0], Workout1P[1], int.Parse(Workout1P[2]));
+            Workout workout2 = new Workout(workout2P[0], workout2P[1], int.Parse(workout2P[2]));
+            Diet diet = new Diet(DietP[0], DietP[1], DietP[2], DietP[3], int.Parse(DietP[4]));
+            Reading reading = new Reading(ReadingP[0], ReadingP[1], int.Parse(ReadingP[2]));
+            WaterLog waterLog = new WaterLog(WaterLogP[0], int.Parse(WaterLogP[1]));
+            DailyJournal dailyJournal = new DailyJournal(DateTime.Parse(DailyinfoP[0]),DailyinfoP[1],workout1,workout2,diet,reading,waterLog, int.Parse(DailyinfoP[2]));
+            AddDailyJournal(dailyJournal);
         }
     }
 
